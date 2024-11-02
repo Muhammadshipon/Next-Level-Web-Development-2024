360f09ae9588de0a2e7fdeeed948f9fcec9a39fe
Muhammad <shalauddinahmedshipon@gmail.com>
2024-11-01 22:53:33 -0800

    Explore Basic Types of TypeScript

--- TypeScript-Technocrat/Module-1/src/index.ts
+++ TypeScript-Technocrat/Module-1/src/index.ts
@@ -64,7 +64,7 @@ const arr:number[] = [1,2,5];
 const newArr: number[] = arr.map((item:number): number =>item*item);
 
 // spread operator 
-const friends:string[] =['rasel','Abdur Rahim', 'nahid'];
+const friends:string[] =['rasel','Abdur Rahim', 'nahid','aslam','masrul'];
 const relativeFriends:string[]=['Foysal','Jibon','Sojib'];
 friends.push(...relativeFriends);
 
@@ -89,3 +89,100 @@ const userInformation ={
 
 }
 const {userId,address,name:{firstName}} = userInformation;
+
+// destructuring
+// const friends:string[] =['rasel','Abdur Rahim', 'nahid'];
+const [,,ogamaraFriend,...rest] = friends;
+
+// type alias 
+type Student = {
+  id: number;
+  name: string;
+  contact?:string;
+  gender: string
+}
+type isProgrammer =boolean;
+type age =number;
+
+const student1 : Student ={
+  id:11,
+  name:'Shipon',
+  contact:'09385',
+  gender:'male'
+}
+const student2 : Student ={
+  id:12,
+  name:'Shipon Ahmed',
+  contact:'09385',
+  gender:'male'
+}
+
+const shiponAge:age = 26;
+const programmer: isProgrammer = true;
+
+// union and intersection type 
+type User ={
+  name: string;
+  email?: string;
+  gender: 'male'|'female';
+  blood_group: 'o+'|'o-';
+}
+
+const user1: User={
+  name:'shipon',
+  gender:'male',
+  blood_group:'o+'
+}
+
+type Frontend = 'fakibaz'| 'jr. frontend developer';
+type Fullstack = 'frontend developer' | 'expert developer';
+type Developer = Frontend | Fullstack ;
+
+type FrontendDeveloper ={
+  skill: string[],
+  designation1: 'frontend developer'
+}
+type BackendDeveloper ={
+  skill: string[],
+  designation2: 'backend developer'
+}
+
+type FullstackDeveloper = FrontendDeveloper & BackendDeveloper;
+
+const developer : FullstackDeveloper ={
+  skill: ['html','css','node.js'],
+  designation1:'frontend developer',
+  designation2:'backend developer'
+}
+console.log('here is ');
+
+// nullish coalescing operator 
+const isAuthenticated = '';
+const result1 = isAuthenticated ?? 'Guest';
+const result2 = isAuthenticated? isAuthenticated:'guest';
+console.log({result1},{result2});
+
+// unknown type 
+const getTopSpeed =(speed:unknown)=>{
+  if(typeof speed ==='number'){
+    return `The top speed is ${speed} km/h`
+  }
+  else if(typeof speed==='string'){
+    const topSpeed = speed.split(' ')[0];
+    const unit = speed.split(' ')[1];
+    return `The top speed is ${topSpeed} ${unit}`
+  }
+  else{
+    return 'please provide correct input'
+  }
+}
+
+
+console.log(getTopSpeed( 133));
+
+// Never type 
+const error =(msg:string):never=>{
+  throw new Error(msg);
+}
+
+console.log(error('mama vule error khaiya gesi'));
